aa64ec38591c64ce3939e9884d01fc2a9e70f744
Mastermind <mastermindx33@gmail.com>
2022-09-26 20:21:39 +0530

    datatypes

---
+++ Datatypes.js
@@ -0,0 +1,19 @@
+let length = 10;  // Number
+let lastname = "Jerish"; // String
+let x = { // Object
+    firstName: "John",
+    lastName: "Doe"
+}
+
+// Arrays
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Objects
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+
+console.log(typeof "");
