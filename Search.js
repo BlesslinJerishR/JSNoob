f791f9871154615fbe9103119955e9fa0a2930d6
Mastermind <mastermindx33@gmail.com>
2022-09-28 15:44:00 +0530

    HTML JS Template Literals

---
+++ Search.js
@@ -0,0 +1,5 @@
+let fruit = "apple";
+let aIn = fruit.indexOf("a");
+let lIn = fruit.indexOf("le");
+console.log(aIn);
+console.log(lIn);
---
+++ TemplateLiterals.js
@@ -0,0 +1,18 @@
+let text = `Hello World`;
+console.log(text);
+let text1 = `Hello "Apple"`;
+console.log(text1);
+
+let text2 =`1
+2
+2
+3
+4`
+
+console.log(text2);
+
+let firstName = "John";
+let lastName = "Doe";
+
+let welcome = `Welcome ${firstName} ${lastName}`;
+console.log(welcome);
