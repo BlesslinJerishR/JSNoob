a7c0723dc43e36af3c6735d07ecbf6cbfa1c9cec
Mastermind <mastermindx33@gmail.com>
2022-09-28 12:35:10 +0530

    slice, substr, replace

---
+++ methods.js
@@ -0,0 +1,12 @@
+//  Slice
+let str = "Apple, Banana, Orange";
+let part = str.slice(7, 13);
+let subPart = str.substring(7, 13);
+
+console.log(part);
+console.log(subPart);
+
+// Negative Slice
+let negPart = str.slice(-6)
+console.log(negPart);
+
